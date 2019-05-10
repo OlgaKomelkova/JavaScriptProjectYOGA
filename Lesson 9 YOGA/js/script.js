cd072cc05c52969bb3c9625eb398cd3dc119eec0
OlgaKomelkova <47720988+OlgaKomelkova@users.noreply.github.com>
2019-05-10 22:25:59 +0500

    Add files via upload

--- Lesson 9 YOGA/js/script.js
+++ Lesson 9 YOGA/js/script.js
@@ -40,7 +40,7 @@ let deadline = '2018-10-21';
 
 function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
-        seconds = Math.floor((t / 1000)),
+        seconds = Math.floor((t / 1000) % 60),
         minutes = Math.floor((t / 1000 / 60) % 60),
         hours = Math.floor((t / (1000 * 60 * 60)));
 
