330b7e6e82fa2ced774316fcde44cd711043cbea
Olga <olgakomelkova@mail.ru>
2019-05-19 10:42:01 +0500

    Update script.js

--- Lesson 12 YOGA  /js/script.js
+++ Lesson 12 YOGA  /js/script.js
@@ -126,56 +126,57 @@
  statusMessage.classList.add('status');
 
  function sendForm(elem) {
-     elem.addEventListener('submit', function(e) {
+     elem.addEventListener('submit', function (e) {
          e.preventDefault();
          elem.appendChild(statusMessage);
 
          let formData = new FormData(elem);
 
-         function postData() {
-            return new Promise(function(resolve,reject) {
-                let request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
-                request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+         function postData(data) {
+             return new Promise(function (resolve, reject) {
+                 let request = new XMLHttpRequest();
+                 request.open('POST', 'server.php');
+                 request.setRequestHeader('Content-Type', 'application/json; charset = utf-8');
 
-                request.onreadystatechange = function(){
-                    if (request.readyState < 4) {
-                        resolve();
-                    } else if (request.readyState === 4) {
-                        if (request.status == 200 && request.status < 300) {
-                            resolve();
-                        } 
-                        else {
-                            reject();
-                        }
-                    }
-               };
+                 let obj = {};
+                 formData.forEach(function (value, key) {
+                     obj[key] = value;
+                 });
+                 let json = JSON.stringify(obj);
 
-               request.send(data);
-            });
-        }
+                 request.onreadystatechange = function () {
+                     if (request.readyState < 4) {
+                         resolve();
+                     } else if (request.readyState === 4) {
+                         if (request.status == 200 && request.status < 300) {
+                             resolve();
+                         } else {
+                             reject();
+                         }
+                     }
+                 };
+                 request.send(json);
+             });
+         }
 
-        function clearInput() {
-            for (let i = 0; i < input.length; i++) {
-                input[i].value = '';
-            }
-        }
+         function clearInput() {
+             for (let i = 0; i < input.length; i++) {
+                 input[i].value = '';
+             }
+         }
 
-        postData(formData)
-        .then(()=> statusMessage.innerHTML = message.loading)
-        .then(()=> {
-            thanksModal.style.display = 'block';
-            mainModal.style.display = 'none';
-            statusMessage.innerHTML = '';
-        })
-        .catch(()=> statusMessage.innerHTML = message.failure)
-        .then(clearInput);
-    });
+         postData(formData)
+             .then(() => statusMessage.innerHTML = message.loading)
+             .then(() => statusMessage.innerHTML = message.succes)
+             .catch(() => statusMessage.innerHTML = message.failure)
+             .then(clearInput);
+     });
+ }
+ sendForm(form);
+ sendForm(contactForm);
 
-    sendForm(form);
-    sendForm(contactForm);
-}
- let inputsTel = document.querySelectorAll('input[name="phone"]');
+
+ let inputsTel = document.querySelectorAll('input[type="tel"]');
 
  function onlyNumbers(input) {
      input.onkeydown = function () {
