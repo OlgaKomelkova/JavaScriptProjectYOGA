a3aa0d32fd8af3201661c846ac30dee58bd793e1
Olga <olgakomelkova@mail.ru>
2019-05-19 09:43:02 +0500

    Update script.js

--- Lesson 11 YOGA  /js/script.js
+++ Lesson 11 YOGA  /js/script.js
@@ -133,36 +133,37 @@
          let request = new XMLHttpRequest();
          request.open('POST', 'server.php');
 
-         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+         request.setRequestHeader('Content-Type', 'application/json; charset = utf-8');
 
          let formData = new FormData(elem);
 
-         request.send(formData);
+         let obj = {};
+         formData.forEach(function(value, key){
+             obj[key] = value;
+         });
+         let json = JSON.stringify(obj);
 
-         request.onreadystatechange = function() {
+         request.send(json);
+
+         request.addEventListener('readystatechange', function() {
              if (request.readyState < 4) {
                  statusMessage.innerHTML = message.loading;
-             } else if (request.readyState === 4) {
-                 if (request.status == 200 && request.status < 300) {
-                     thanksModal.style.display = 'block';
-                     mainModal.style.display = 'none';
-                     statusMessage.innerHTML = '';
-                 } 
-                 else {
-                     statusMessage.innerHTML = message.failure;
-                 }
+             } else if (request.readyState === 4 && request.status == 200) {
+                 statusMessage.innerHTML = message.succes;
+             } else {
+                    statusMessage.innerHTML = message.failure;
              }
-         };
-
-         for (let i = 0; i < input, lenght; i++) {
+         });
+         for (let i = 0; i < input.lenght; i++) {
              input[i].value = '';
          }
      });
- }
+    }
+    
  sendForm(form);
  sendForm(contactForm);
 
- let inputsTel = document.querySelectorAll('input[name="phone"]');
+ let inputsTel = document.querySelectorAll('input[type="tel"]');
 
  function onlyNumbers(input) {
      input.onkeydown = function () {
@@ -171,3 +172,4 @@
  }
 
  [...inputsTel].forEach(elem => onlyNumbers(elem));
+ 
