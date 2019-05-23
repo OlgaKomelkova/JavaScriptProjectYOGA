69cc5b62acf7a70f35fc1dd62bb5e70cc84fe90d
Olga <olgakomelkova@mail.ru>
2019-05-23 23:22:20 +0500

    После замечаний

---
+++ yoga/src/parts/calc.js
@@ -0,0 +1,61 @@
+ /*jshint esversion: 6 */
+ function calc() {
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+totalValue.innerHTML = 0;
+
+persons.addEventListener('change', function() {
+    personsSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if(restDays.value == '') {
+        totalValue.innerHTML = 0;
+    } else {
+        totalValue.innerHTML = total;
+    }
+});
+
+restDays.addEventListener('change', function() {
+    daysSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if(persons.value == '') {
+        totalValue.innerHTML = 0;
+    } else {
+        totalValue.innerHTML = total * place.options[place.selectedIndex].value;
+    }
+});
+
+place.addEventListener('change', function() {
+    if(restDays.value == '' || persons.value == '') {
+        totalValue.innerHTML = 0;
+    } else {
+        let a = total;
+        totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+    }
+});
+
+let inputsCounter = document.querySelectorAll('.counter-block-input');
+
+
+
+[].forEach.call(inputsCounter, (el =>{
+el.addEventListener('input', evt =>{
+  if ( !el.oldValue ) {el.oldValue = '';} 
+
+  if (/^\d+$/.test(el.value) || el.value === ' ' ){
+    el.oldValue = el.value;
+  } else {
+    el.value = el.value;
+  }
+});
+}));
+ }
+
+ module.exports = calc;
