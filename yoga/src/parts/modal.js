64405e5dbaacaeb5251e9ce30ae01bb9feda24d2
Olga <olgakomelkova@mail.ru>
2019-05-24 10:19:25 +0500

    Исправления 24.05

---
+++ yoga/src/parts/modal.js
@@ -0,0 +1,27 @@
+ /*jshint esversion: 6 */
+ function modal() {
+ let more = document.body.getElementsByClassName('more')[0],
+     overlay = document.body.getElementsByClassName('overlay')[0],
+     close = document.body.getElementsByClassName('popup-close')[0],
+     descriptionBtns = document.body.getElementsByClassName('description-btn');
+
+ function showModalPopup() {
+     overlay.style.display = 'block';
+     this.classList.add('more-splash');
+     document.body.style.overflow = 'hidden';
+ }
+
+ more.addEventListener('click', showModalPopup);
+
+ for (let i = 0; i < descriptionBtns.length; i++) {
+     descriptionBtns[i].addEventListener('click', showModalPopup);
+ }
+
+ close.addEventListener('click', function () {
+     overlay.style.display = 'none';
+     this.classList.remove('more-spalsh');
+     document.body.style.overflow = '';
+ });
+ }
+
+ module.exports = modal;
